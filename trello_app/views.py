55816a473a9594ffc7a00f385a371389fa5e3428
Naoki Imai <naokinetin0110@gmail.com>
2021-01-11 14:51:22 +0900

    create users

--- trello_app/views.py
+++ trello_app/views.py
@@ -1,12 +1,19 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect, resolve_url
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
+from django.views.generic import DetailView, UpdateView
+from .forms import UserForm
+from .mixins import OnlyYouMixin
+
 
 # Create your views here.
 def index(request):
   return render(request, "trello_app/index.html")
 
+#ログインしたらhomeへリダイレクトする
 @login_required
 def home(request):
   return render(request, "trello_app/home.html")
@@ -25,3 +32,15 @@ def signup(request):
     "form":form
   }
   return render(request, 'trello_app/signup.html', context)
+
+class UserDetailView(LoginRequiredMixin, DetailView):
+  model = User
+  template_name = "trello_app/users/detail.html"
+
+class UserUpdateView(OnlyYouMixin,UpdateView):
+  model = User
+  template_name = "trello_app/users/update.html"
+  form_class = UserForm
+# resolve_url 正常ならusers_detaiに接続する.pkはDBのpk
+  def get_success_url(self):
+    return resolve_url('trello_app:users_detail', pk=self.kwargs['pk'])
