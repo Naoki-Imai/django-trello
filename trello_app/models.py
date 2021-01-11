453c2240db56db2fc39626a131ee26d4158a4bf3
Naoki Imai <naokinetin0110@gmail.com>
2021-01-11 18:42:24 +0900

    create list

--- trello_app/models.py
+++ trello_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class List(models.Model):
+  title = models.CharField(max_length=300)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.title
+  
 
 # Create your models here.
--- trello_app/views.py
+++ trello_app/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render,redirect, resolve_url
+from django.urls import reverse_lazy
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
-from django.views.generic import DetailView, UpdateView
-from .forms import UserForm
+from django.views.generic import DetailView, UpdateView, CreateView, ListView, DeleteView
+
+from .forms import UserForm, ListForm
+from .models import List
 from .mixins import OnlyYouMixin
 
 
@@ -44,3 +47,35 @@ class UserUpdateView(OnlyYouMixin,UpdateView):
 # resolve_url 正常ならusers_detaiに接続する.pkはDBのpk
   def get_success_url(self):
     return resolve_url('trello_app:users_detail', pk=self.kwargs['pk'])
+
+class ListCreateView(LoginRequiredMixin, CreateView):
+  model = List
+  template_name = 'trello_app/lists/create.html'
+  form_class = ListForm
+  success_url = reverse_lazy('trello_app:lists_list')
+
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
+
+class ListListView(LoginRequiredMixin, ListView):
+  model = List
+  template_name = 'trello_app/lists/list.html'
+
+class ListDetailsView(LoginRequiredMixin, DetailView):
+  model = List
+  template_name = 'trello_app/lists/detail.html'
+
+class ListUpdateView(LoginRequiredMixin,UpdateView):
+  model = List
+  template_name = "trello_app/lists/update.html"
+  form_class = ListForm
+# resolve_url 正常ならusers_detaiに接続する.pkはDBのpk
+  def get_success_url(self):
+    return resolve_url('trello_app:lists_detail', pk=self.kwargs['pk'])
+
+class ListDeleteView(LoginRequiredMixin, DeleteView):
+  model = List
+  template_name = 'trello_app/lists/delete.html'
+  form_class = ListForm
+  success_url = reverse_lazy('trello_app:lists_list')
